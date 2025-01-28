294c28bf8be1a072cee6d815302efddcf20e2520
ElierEstopinan <152028521+ElierEstopinan@users.noreply.github.com>
2025-01-28 19:58:40 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,27 @@
+function skillsMember() {
+    // skills
+    var skills = [
+        "HTML5",
+        "CSS3",
+        "JavaScript",
+        "jQuery",
+        "AngularJS",
+        "Node.js",
+        "Express.js",
+        "MongoDB",
+        "PHP",
+        "MySQL",
+        "Sass",
+        "Grunt",
+        "Gulp",
+        "Git",
+        "WordPress",
+        "Photoshop",
+        "Illustrator",
+        "Sketch",
+        "InVision",
+        "Zeplin"
+    ];
+    // return
+    return skills;
+}
